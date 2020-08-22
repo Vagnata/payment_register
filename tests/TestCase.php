d3bec702b5457923d6de8aa7e28ed0b7eccaf05a
vagnata <garciavagnata.vg@gmail.com>
2020-08-22 01:19:23 -0400

    Adiciona provider company ao faker

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Faker\Generator;
+use Faker\Provider\pt_BR\Company;
 use Faker\Provider\pt_BR\Internet;
 use Faker\Provider\pt_BR\Person;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -21,6 +22,7 @@ abstract class TestCase extends BaseTestCase
         $this->faker = new Generator();
         $this->faker->addProvider(new Person($this->faker));
         $this->faker->addProvider(new Internet($this->faker));
+        $this->faker->addProvider(new Company($this->faker));
         Artisan::call('migrate');
         $this->seed();
     }
