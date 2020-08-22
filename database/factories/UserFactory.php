ce56d30fe91d177ee3ebd6b1f0fcb86936e5509c
vagnata <garciavagnata.vg@gmail.com>
2020-08-22 18:56:54 -0400

    Ajusta UserFactory para utilizar em testes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,27 +2,24 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\User;
+use App\Domain\Enuns\UserTypesEnum;
+use App\Domain\Models\User;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
-
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
+use Faker\Provider\pt_BR\Company;
+use Faker\Provider\pt_BR\Internet;
+use Faker\Provider\pt_BR\Person;
 
 $factory->define(User::class, function (Faker $faker) {
+    $faker->addProvider(new Person($this->faker));
+    $faker->addProvider(new Internet($this->faker));
+    $faker->addProvider(new Company($this->faker));
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'name'         => $this->faker->name,
+        'cpf'          => $this->faker->cpf(false),
+        'email'        => $this->faker->email,
+        'cnpj'         => $this->faker->cnpj(false),
+        'password'     => $this->faker->password,
+        'user_type_id' => $this->faker->randomElement(UserTypesEnum::toArray())
     ];
 });
