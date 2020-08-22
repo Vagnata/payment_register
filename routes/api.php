4d22055d87dafd307b9244a1ddb9b319e05bea58
vagnata <garciavagnata.vg@gmail.com>
2020-08-21 23:00:12 -0400

    Implementa rota de cadastro de usuário

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::get('/', function (Request $request) {
     return trans('messages.home.welcome');
+});
+
+Route::prefix('user')->group(function () {
+    Route::post('/', 'UserController@post');
 });
