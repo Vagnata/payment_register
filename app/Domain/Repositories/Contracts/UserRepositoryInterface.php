8287c41d5a42023470814a560f4fdc44d1c9bfb2
vagnata <garciavagnata.vg@gmail.com>
2020-08-23 00:21:10 -0400

    Ajusta interfaces para conter os métodos da interface abstrata

--- app/Domain/Repositories/Contracts/UserRepositoryInterface.php
+++ app/Domain/Repositories/Contracts/UserRepositoryInterface.php
@@ -3,8 +3,11 @@
 namespace App\Domain\Repositories\Contracts;
 
 use App\Domain\Models\User;
+use Illuminate\Database\Eloquent\Model;
 
 interface UserRepositoryInterface
 {
     public function findById(int $userId): ?User;
+    public function save(array $attributes): Model;
+    public function update(Model $model, array $attributes): Model;
 }
--- app/Domain/Repositories/Contracts/WalletRepositoryInterface.php
+++ app/Domain/Repositories/Contracts/WalletRepositoryInterface.php
@@ -3,8 +3,11 @@
 namespace App\Domain\Repositories\Contracts;
 
 use App\Domain\Models\Wallet;
+use Illuminate\Database\Eloquent\Model;
 
 interface WalletRepositoryInterface
 {
     public function findByUserId(int $userId): ?Wallet;
+    public function save(array $attributes): Model;
+    public function update(Model $model, array $attributes): Model;
 }
