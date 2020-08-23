db87427df5a8d2d95aba305a39f183870890bfa4
vagnata <garciavagnata.vg@gmail.com>
2020-08-23 19:31:05 -0400

    Implementa provider para definir serviços de integração

---
+++ app/Providers/IntegrationProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Providers;
+
+use App\Domain\Integrations\PaymentIntegrationService;
+use GuzzleHttp\Client;
+use Illuminate\Support\ServiceProvider;
+
+class IntegrationProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton(PaymentIntegrationService::class, function ($app) {
+            return new PaymentIntegrationService(new Client(config('services.payment_integration')));
+        });
+    }
+}
