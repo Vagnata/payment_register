23c4e6ae62ab9cc01a0418745778d193af44d058
vagnata <garciavagnata.vg@gmail.com>
2020-08-23 17:45:07 -0400

    Implementa transaction factory para criação de testes

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Domain\Enuns\TransactionStatusEnum;
+use App\Domain\Models\Transaction;
+use App\Domain\Models\Wallet;
+use Faker\Generator as Faker;
+
+$factory->define(Transaction::class, function (Faker $faker) {
+    $payerWallet = Wallet::first() ?? factory(Wallet::class)->create();
+    $payeeWallet = Wallet::last() ?? factory(Wallet::class)->create();
+    return [
+        'payer_wallet_id'       => $payerWallet->id,
+        'payee_wallet_id'       => $payeeWallet->id,
+        'amount'                => 000.00,
+        'transaction_status_id' => $faker->randomElement(TransactionStatusEnum::toArray())
+    ];
+});
