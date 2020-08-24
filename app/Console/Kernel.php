b6b3df27ffbde728755f222784ac6800ebcbbf2e
vagnata <garciavagnata.vg@gmail.com>
2020-08-23 23:42:33 -0400

    Adiciona command ao scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\NotificationSenderCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        NotificationSenderCommand::class
     ];
 
     /**
@@ -24,7 +25,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(NotificationSenderCommand::class)
+            ->everyMinute()
+            ->appendOutputTo(storage_path().'/logs/notifications.log');
     }
 
     /**
